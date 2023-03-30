9491aed7f85b028216fddca7fb00537f40210e96
Andrey Lebedev <alebedev@redhat.com>
2023-03-30 20:42:45 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/alebedev87/another-module-uses-mongo-driver/pkg/output"
+	nodeps "github.com/alebedev87/another-module-uses-mongo-driver/pkg/outputnodeps"
+)
+
+func main() {
+	output.Output()
+	nodeps.Output()
+}
